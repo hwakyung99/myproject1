9a4a9286ec6435855b0b21843f5c23f3a5e3038d
hwakyung <hwakyung5531@gmail.com>
2024-11-25 13:57:21 +0900

    저장소 초기화

---
+++ Hello.java
@@ -0,0 +1,9 @@
+public class Hello {
+	public static void main(String[] args) {
+		System.out.println("Hello world");
+		System.out.println("Hello world2");
+		System.out.println("Hello world3");
+		System.out.println("Hello world4");
+		System.out.println("Hello world5");
+	}
+}
